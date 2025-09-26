ec1c66ebfe147c8c5881f2259c66ff7fd5436604
Johannes Schmidt <johannes.schmidt@icinga.com>
2025-09-26 10:17:14 +0200

    Fix TestLogger not updating severity by itself
    MinSeverity only gets updated if the logger is already active, so
    we have to set severity after the logger has been activated.
    
    Also add a Clear() method to clear existing log messages and
    assert macros to assert both the existence and absence of a pattern
    in the log.

--- test/base-testloggerfixture.hpp
+++ test/base-testloggerfixture.hpp
@@ -10,6 +10,12 @@
 #include <boost/test/test_tools.hpp>
 #include <future>
 
+#define CHECK_LOG_MESSAGE(pattern, timeout) BOOST_CHECK(ExpectLogPattern(pattern, timeout))
+#define REQUIRE_LOG_MESSAGE(pattern, timeout) BOOST_REQUIRE(ExpectLogPattern(pattern, timeout))
+
+#define CHECK_NO_LOG_MESSAGE(pattern, timeout) BOOST_CHECK(!ExpectLogPattern(pattern, timeout))
+#define REQUIRE_NO_LOG_MESSAGE(pattern, timeout) BOOST_REQUIRE(!ExpectLogPattern(pattern, timeout))
+
 namespace icinga {
 
 class TestLogger : public Logger
@@ -52,6 +58,13 @@ public:
 		return ret;
 	}
 
+	void Clear()
+	{
+		std::unique_lock lock(m_Mutex);
+		m_Expects.clear();
+		m_LogEntries.clear();
+	}
+
 private:
 	void ProcessLogEntry(const LogEntry& entry) override
 	{
@@ -87,9 +100,9 @@ struct TestLoggerFixture
 {
 	TestLoggerFixture()
 	{
-		testLogger->SetSeverity(testLogger->SeverityToString(LogDebug));
 		testLogger->Activate(true);
 		testLogger->SetActive(true);
+		testLogger->SetSeverity(TestLogger::SeverityToString(LogDebug));
 	}
 
 	~TestLoggerFixture()
